5e181a48e1de2eb80895de0939f882cedf62d99e
Bognar Junior <bognar_junior@yahoo.com.br>
2016-02-10 20:54:48 -0200

    Bower
    Instalação do Bower

--- Estudos/NodeJS/MEAN/Contatooh/config/express.js
+++ Estudos/NodeJS/MEAN/Contatooh/config/express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 var home = require('./../app/routes/home');
 module.exports = function(){
 	var app = express();
@@ -7,6 +8,11 @@ module.exports = function(){
 	app.set('port', 3000);
 	//Middleware
 	app.use(express.static('./public'));
+	
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+
+	app.use(require('method-override')());
 
 	app.set('view engine', 'ejs');
 
