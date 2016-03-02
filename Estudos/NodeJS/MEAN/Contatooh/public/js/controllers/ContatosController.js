9414d3633eaa3a70c05d9ce9837bfb1cee709a0f
Bognar Junior <bognar_junior@yahoo.com.br>
2016-03-01 22:09:00 -0300

    AngularJS - $http
    Integrando front-end com back-end utilizando o $http

--- Estudos/NodeJS/MEAN/Contatooh/public/js/controllers/ContatosController.js
+++ Estudos/NodeJS/MEAN/Contatooh/public/js/controllers/ContatosController.js
@@ -1,27 +1,14 @@
-angular.module('contatooh').controller('ContatosController',['$scope', function($scope) {
-		$scope.total = 0;
-
+angular.module('contatooh').controller('ContatosController', function($scope, $http) {
 		$scope.filtro = '';
 
-		$scope.incrementa = function(){
-			$scope.total ++;
-		};
+		$scope.contatos = [];
 
-		$scope.contatos = [
-			{
-				"_id": 1,
-				"nome": "Contato Angular 1",
-				"email": "cont1@empresa.com.br"
-			},
-			{
-				"_id": 2,
-				"nome": "Contato Angular 2",
-				"email": "cont2@empresa.com.br"
-			},
-			{
-				"_id": 3,
-				"nome": "Contato Angular 3",
-				"email": "cont3@empresa.com.br"
-			}
-		];
-}]);
+		$http.get('/contatos')
+		.success( function( data ){
+			$scope.contatos = data;
+		})
+		.error( function( statusText ){
+			console.log('Não foi possível obter a lista de contatos!');
+			console.log( statusText);
+		});
+});
