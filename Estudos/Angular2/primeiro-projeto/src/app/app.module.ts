f0e602eea9a0e49904440c804e3977990ea7f986
Bognar Junior <bognar_junior@yahoo.com.br>
2017-11-26 20:25:50 -0200

    Estudos Angular Laravel
    Estudos com Angular e Larave

---
+++ Estudos/Angular2/primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
+
+@NgModule({
+	declarations: [
+	    AppComponent, 
+	    MeuPrimeiroComponent, MeuPrimeiro2Component
+	],
+	imports: [
+		BrowserModule,
+		CursosModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Estudos/Angular2/primeiro-projeto/src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-curso-detalhe',
+  templateUrl: './curso-detalhe.component.html',
+  styleUrls: ['./curso-detalhe.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CursoDetalheComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
