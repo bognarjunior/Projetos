446bb3b6486c578aaa3004135bfdeea3ef672fdc
Bognar Junior <bognar_junior@yahoo.com.br>
2017-12-23 13:50:18 -0200

    Capítulo 5
    Capítulo 5 do livro Cangaceiro JavaScript, criando classe para
    negociações e separando o código do controller

--- Estudos/JavaScript/jscangaceiro/client/app/controllers/NegociacaoController.js
+++ Estudos/JavaScript/jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -7,20 +7,32 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
+
         event.preventDefault();
-        
-        let negociacao = new Negociacao(
+
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._limparFormulario();
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
+    }
 
-        let d = DateConverter.paraTexto(negociacao.data);
-
-        console.log(d);
-        console.log(negociacao.data);
+    _limparFormulario() {
+        
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
