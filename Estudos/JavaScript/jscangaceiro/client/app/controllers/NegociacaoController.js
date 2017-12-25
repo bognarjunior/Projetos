3eb72002578ee7c870cf692a484d64850afa2db2
Bognar Junior <bognar_junior@yahoo.com.br>
2017-12-25 14:50:17 -0200

    Capítulo 10
    Capítulo 10 do livro Cangaceiro JavaScript.
    Utilizando o padrão de projeto Factory, criando dentro do factory nossa
    chamada para o proxy e criando uma classe Bind que será responsável
    pela implementação da classe ProxyFactory, que terá a regra do negócio.
    Retirado da classe NegociacaoController toda a regra de negócio do
    construtor.

--- Estudos/JavaScript/jscangaceiro/client/app/controllers/NegociacaoController.js
+++ Estudos/JavaScript/jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -8,34 +8,17 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        const self = this;
+        this._negociacoes = new Bind(
+            new Negociacoes(),
+            new NegociacoesView('#negociacoes'),
+            'adiciona', 'esvazia'
+        );
 
-        this._negociacoes = new Proxy( new Negociacoes(), {
-
-            get(target, prop, value, receiver) {
-
-                if (typeof(target[prop]) == typeof(Function) 
-                    && ['adiciona', 'esvazia'].includes(prop)) {
-                    
-                        return function() {
-
-                            console.log(`"${prop}" disparou a armadilha`);
-                            target[prop].apply(target, arguments);
-                            self._negociacoesView.update(target);
-                        }
-                } else {
-                    return target[prop];
-                }
-            }
-        });
-
-        this._negociacoesView = new NegociacoesView('#negociacoes');
-        this._negociacoesView.update(this._negociacoes);
-
-        this._mensagem = new Mensagem();
-
-        this._mensagemView = new MensagemView('#mensagemView');
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView('#mensagemView'),
+            'texto'
+        );
     }
 
     _criaNegociacao() {
@@ -48,19 +31,15 @@ class NegociacaoController {
     }
 
     adiciona(event) {
-
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         this._limparFormulario();
     }
 
     apaga() {
-
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
 
     _limparFormulario() {
