122e2979a4f89f9eaba702560a320a2dc655fda6
Bognar Junior <bognar_junior@yahoo.com.br>
2018-01-30 21:19:44 -0200

    Trhottle e Debounce Javascript
    Estudos de Throttle e Debounce com JavaScript

---
+++ blog/JavaScript Debounce Throttle/app.js
@@ -0,0 +1,49 @@
+function loadPage() {
+    this.addEvent();
+}
+
+function addEvent() {
+    document.getElementById("debounce").addEventListener('click', debounce(function() {
+        console.info('Debounce', new Date().toUTCString());
+    }, 3000));
+    
+    document.getElementById("throttle").addEventListener('click', throttle(function() {
+        return console.log('Throttle', new Date().toUTCString());
+    }, 3000));
+}
+const debounce = (fn, delay) => {
+    let timer;
+    return function() {
+        const self = this
+        const args = arguments
+        clearTimeout(timer)
+        timer = setTimeout(() => fn.apply(self, args), delay)
+    }
+}
+
+const throttle = (fn, delay) => {
+
+    let isThrottle, timer, last;
+
+    return function() {
+
+        const context = this;
+        const args = arguments;
+
+        if (!isThrottle) {
+            fn.apply(context, args);
+            last = Date.now();
+            isThrottle = true;
+        } else {
+            clearTimeout(timer)
+            timer = setTimeout(function() {
+                if ((Date.now() - last) >= delay) {
+                    fn.apply(context, args)
+                    last = Date.now()
+                }
+            }, delay - (Date.now() - last))
+        }
+    }
+}
+
+window.onload = loadPage;
