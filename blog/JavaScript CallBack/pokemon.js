e395f87938211e132c4d52dd18b687dd18ae8c44
Bognar Junior <bognar_junior@yahoo.com.br>
2018-02-22 20:39:25 -0300

    Ajustes
    Ajustes

--- blog/JavaScript CallBack/pokemon.js
+++ blog/JavaScript CallBack/pokemon.js
@@ -3,9 +3,9 @@ const url ="https://pokeapi.co/api/v2/pokemon/?limit=5";
 
 let callback = res => {
     res.setEncoding("utf8");
-    let body = [];
+    let body = "";
 
-    res.on("data", data => body.push(data));
+    res.on("data", data => body = data);
     res.on("end", () => console.log(JSON.parse(body)));
 }
 
